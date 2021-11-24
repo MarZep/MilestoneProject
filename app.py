f0bc42c9359f96e14a1b66cf6fabae3908e05e7d
MarZep <mlopez1706@gmail.com>
2021-11-24 00:13:04 -0300

    modificacion lista por lista de diccionarios

--- app.py
+++ app.py
@@ -1,25 +1,31 @@
-def generar_lista_peliculas ():
-    peliculas_lista_inicial = ["Terminator", "Rocky","Halloween"]
-    return peliculas_lista_inicial
+def generar_diccionario_peliculas ():
+    peliculas_diccionario_inicial = [
+        {'pelicula': "Terminator", 'director': "James Cameron", 'año': 1984},
+        {'pelicula': "Rocky", 'director': "Jhon Avildsen", 'año': 1976},
+        {'pelicula': "Halloween", 'director': "John Carpenter", 'año': 1978}
+    ]
+    return peliculas_diccionario_inicial
 
-def agregar_pelicula (peliculas_lista_vfuncion):
+def agregar_pelicula (peliculas_diccionario_vfuncion):
     pelicula_a_agregar = str(input("ingrese pelicula a agregar: \n"))
-    peliculas_lista_vfuncion.append(pelicula_a_agregar)
-    return peliculas_lista_vfuncion
+    director_a_agregar = str(input("ingrese director de la pelicula a agregar: \n"))
+    anio_de_pelicula_agregar = int(input("ingrese director de la pelicula a agregar: \n"))
+    peliculas_diccionario_vfuncion.append({"pelicula": pelicula_a_agregar, "director": director_a_agregar, "año": anio_de_pelicula_agregar})
+    return peliculas_diccionario_vfuncion
 
-def mostrar_peliculas (peliculas_lista_vfuncionmostrar):
-    for valor_lista_peliculas in peliculas_lista_vfuncionmostrar:
-        print(valor_lista_peliculas)
+def mostrar_peliculas (peliculas_diccionario_vfuncionmostrar):
+    for peliculas in peliculas_diccionario_vfuncionmostrar:
+        print(f"Pelicula: {peliculas['pelicula']}    Director: {peliculas['director']}     Año: {peliculas['año']}")
 
 #def buscar_por_titulo()
  #   str(input())
 
 
-def realizar_accion (numero_accion,peliculas_lista):
+def realizar_accion (numero_accion,peliculas_diccionario_vfuncion_raccion):
     if numero_accion == 1:
-        peliculas_lista = agregar_pelicula(peliculas_lista)
+        agregar_pelicula(peliculas_diccionario_vfuncion_raccion)
     elif numero_accion == 2:
-        mostrar_peliculas(peliculas_lista)
+        mostrar_peliculas(peliculas_diccionario_vfuncion_raccion)
     elif numero_accion == 3:
                  """funcion4"""
     else: "Salir"
@@ -32,11 +38,11 @@ mensaje = "Seleccionar accion a realizar: 1.Agregar pelicula 2.Mostrar todas mis
           "3.Buscar por titulo 4.Salir \n"
 accion = 0
 
-peliculas_lista = generar_lista_peliculas()
+peliculas_diccionario = generar_diccionario_peliculas()
 
 while accion != 4:
     accion = int(input(mensaje))
-    realizar_accion(accion, peliculas_lista)
+    realizar_accion(accion, peliculas_diccionario)
 
 """for vueltas in range(2):
     try:
